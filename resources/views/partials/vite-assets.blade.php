d36fc0af2128163c2146c5eee19f21170c0412a4
Inès Gandaho <gandahobrunelle@gmail.com>
2026-08-20 12:17:39 +0100

    fix: use relative Vite asset URLs to fix CSS/JS loading on Railway

--- resources/views/partials/vite-assets.blade.php
+++ resources/views/partials/vite-assets.blade.php
@@ -5,28 +5,28 @@
     if (file_exists($manifestPath)) {
         $manifest = json_decode(file_get_contents($manifestPath), true);
         if (isset($manifest['resources/css/app.css']['file'])) {
-            $viteCss = 'build/' . $manifest['resources/css/app.css']['file'];
+            $viteCss = $manifest['resources/css/app.css']['file'];
         }
         if (isset($manifest['resources/js/app.js']['file'])) {
-            $viteJs = 'build/' . $manifest['resources/js/app.js']['file'];
+            $viteJs = $manifest['resources/js/app.js']['file'];
         }
     }
     if (!$viteCss) {
-        $cssFiles = glob(public_path('build/assets/app-*.css'));
+        $cssFiles = glob(public_path('build/assets/app*.css'));
         if (!empty($cssFiles)) {
             $viteCss = 'build/assets/' . basename($cssFiles[0]);
         }
     }
     if (!$viteJs) {
-        $jsFiles = glob(public_path('build/assets/app-*.js'));
+        $jsFiles = glob(public_path('build/assets/app*.js'));
         if (!empty($jsFiles)) {
             $viteJs = 'build/assets/' . basename($jsFiles[0]);
         }
     }
 @endphp
 @if($viteCss)
-    <link rel="stylesheet" href="{{ asset($viteCss) }}">
+    <link rel="stylesheet" href="/build/{{ ltrim($viteCss, '/') }}">
 @endif
 @if($viteJs)
-    <script type="module" src="{{ asset($viteJs) }}"></script>
+    <script type="module" src="/build/{{ ltrim($viteJs, '/') }}"></script>
 @endif
